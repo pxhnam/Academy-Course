801b590933f5409bddad78faf9f7cb2060cc941a
Phan Nam <pxhnam@gmail.com>
2024-06-22 16:44:59 +0700

    init commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Trang Chu</title>
 </head>
 
 <body>
-    <img src="{{ asset('storage/images/html.jpg') }}" alt="Example Image">
-
+    <h3>WELCOME TO WEBSITE ACADEMY</h3>
+    {{-- <img src="{{ asset('storage/images/html.jpg') }}" alt="Example Image"> --}}
 </body>
 
 </html>
